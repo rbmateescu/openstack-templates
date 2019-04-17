ffaaa18d67fcb4ed546f0d90c0a6b51dc21e82c0
mateescu <mateescu@ca.ibm.com>
2019-04-17 10:39:22 -0400

    null_resource

--- terraform/import-vm/main.tf
+++ terraform/import-vm/main.tf
@@ -1,3 +1,5 @@
-  provisioner "local-exec" {
-    command = "chmod 755 ${path.module}/scripts/import_vm.sh && ${path.module}/scripts/import_vm.sh -u ${var.cam_username} -p ${var.cam_password} -i ${var.cam_instancename} -n ${var.cam_namespace} -c ${var.cam_cloudconnection_id} -t ${var.cam_template_id} -o ${var.cloud_instance_id}"
+  resource "null_resource" "ImportVM" {
+    provisioner "local-exec" {
+      command = "chmod 755 ${path.module}/scripts/import_vm.sh && ${path.module}/scripts/import_vm.sh -u ${var.cam_username} -p ${var.cam_password} -i ${var.cam_instancename} -n ${var.cam_namespace} -c ${var.cam_cloudconnection_id} -t ${var.cam_template_id} -o ${var.cloud_instance_id}"
+    }
   }
