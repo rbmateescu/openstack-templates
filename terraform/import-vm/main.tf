cd4ae74d3ae9b1d148a75593fe63308a7f46a6f6
mateescu <mateescu@ca.ibm.com>
2019-04-17 16:52:57 -0400

    import

--- terraform/import-vm/main.tf
+++ terraform/import-vm/main.tf
@@ -1,12 +1,26 @@
-resource "null_resource" "ImportVM" {
-  provisioner "local-exec" {
-    command = "chmod 755 ${path.module}/scripts/import_vm_by_id.sh && ${path.module}/scripts/import_vm_by_id.sh -h ${var.cam_hostname} -u ${var.cam_username} -p ${var.cam_password} -i ${var.cam_instancename} -n ${var.cam_namespace} -c ${var.cam_cloudconnection_id} -t ${var.cam_template_id} -o ${var.cloud_instance_id}"
-  }
-}
+#resource "null_resource" "ImportVM" {
+#  provisioner "local-exec" {
+#    command = "chmod 755 ${path.module}/scripts/import_vm_by_id.sh && ${path.module}/scripts/import_vm_by_id.sh -h ${var.cam_hostname} -u ${var.cam_username} -p ${var.cam_password} -i ${var.cam_instancename} -n ${var.cam_namespace} -c ${var.cam_cloudconnection_id} -t ${var.cam_template_id} -o ${var.cloud_instance_id}"
+#  }
+#}
 
-resource "camc_scriptpackage" "FetchIPV4" {
-  depends_on = ["null_resource.ImportVM"]
+#resource "camc_scriptpackage" "FetchIPV4" {
+#  depends_on = ["null_resource.ImportVM"]
   
-  program = ["/bin/bash", "svrInfo=$(cat ./ipv4);", "if [ -z $svrInfo ]; then echo '{}'; else echo $svrInfo; fi"]
-  on_create = true
+#  program = ["svrInfo=$(cat ./ipv4);", "if [ -z $svrInfo ]; then echo '{}'; else echo $svrInfo; fi"]
+#  on_create = true
+#}
+
+
+data "external" "import" {
+  program = ["/bin/bash", "${path.module}/scripts/import_vm_by_id_external.sh"]
+  query = {
+    host  = "${var.cam_hostname}"
+    password = "${var.cam_password}"
+    instance-name = "${var.cam_instancename}"
+    instance-namespace = "${var.cam_namespace}"
+    cloud-connection-id = "${var.cam_cloudconnection_id}"
+    template-id = "${var.cam_template_id}"
+    id-from-provider = "${var.cloud_instance_id}"
+  }
 }
--- terraform/import-vm/outputs.tf
+++ terraform/import-vm/outputs.tf
@@ -2,11 +2,7 @@
 #  description = "URL of the imported CAM instance"
 #}
 
-locals {
-  ipv4   = "${camc_scriptpackage.FetchIPV4.result}"
-}
-
 output "imported_vm_ipv4"
 {
-  value = "${local.ipv4}"
+  value = "${data.external.import.result}"
 }
