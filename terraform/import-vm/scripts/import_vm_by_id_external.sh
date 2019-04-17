7fc9ea4270d976dd5782547181d806554ea2d759
mateescu <mateescu@ca.ibm.com>
2019-04-17 17:49:23 -0400

    cc

--- terraform/import-vm/scripts/import_vm_by_id_external.sh
+++ terraform/import-vm/scripts/import_vm_by_id_external.sh
@@ -3,9 +3,9 @@
 set -x
 
 # Get script parameters
-eval "$(jq -r '@sh "PARAM_CAM_IP=\(.host_name) PARAM_AUTH_USER=\(.user_name) PARAM_AUTH_PASSWORD=\(.password) PARAM_INSTANCE_NAME=\(.instance_name)"')"
+eval "$(jq -r '@sh "PARAM_CAM_IP=\(.host_name) PARAM_AUTH_USER=\(.user_name) PARAM_AUTH_PASSWORD=\(.password) PARAM_INSTANCE_NAME=\(.instance_name) PARAM_INSTANCE_NAMESPACE=\(.instance_namespace) PARAM_CC_NAME=\(.cloud_connection_id)"')"
 #eval "$(jq -r '@sh "IP_ADDRESS=\(.ip_address) PRIVATE_KEY=\(.private_key)"')"
-#PARAM_INSTANCE_NAMESPACE=\(.instance_namespace) PARAM_CC_NAME=\(.cloud_connection_id) PARAM_TEMPLATE_NAME=\(.template_id) PARAM_ID_FROM_PROVIDER=\(.id_from_provider)
+# PARAM_TEMPLATE_NAME=\(.template_id) PARAM_ID_FROM_PROVIDER=\(.id_from_provider)
 #eval "$(jq -r '@sh "PARAM_AUTH_USER=\(.user)"')"
 #eval "$(jq -r '@sh "PARAM_AUTH_PASSWORD=\(.password)"')"
 #eval "$(jq -r '@sh "PARAM_INSTANCE_NAME=\(.instance_name)"')"
