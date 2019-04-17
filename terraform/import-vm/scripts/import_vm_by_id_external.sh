f5a445a9b9eaddbcf421385150a8b82c2056baf5
mateescu <mateescu@ca.ibm.com>
2019-04-17 18:11:14 -0400

    ewq

--- terraform/import-vm/scripts/import_vm_by_id_external.sh
+++ terraform/import-vm/scripts/import_vm_by_id_external.sh
@@ -19,5 +19,14 @@ PARAM_ID_FROM_PROVIDER=\(.id_from_provider)"')"
 #eval "$(jq -r '@sh "PARAM_TEMPLATE_NAME=\(.template_id)"')"
 #eval "$(jq -r '@sh "PARAM_ID_FROM_PROVIDER=\(.id_from_provider)"')"
 
+printf "\033[33m [PARAM_CAM_IP: $PARAM_CAM_IP]\n\033[0m\n"
+printf "\033[33m [PARAM_AUTH_USER: $PARAM_AUTH_USER]\n\033[0m\n"
+printf "\033[33m [PARAM_INSTANCE_NAME: $PARAM_INSTANCE_NAME]\n\033[0m\n"
+printf "\033[33m [PARAM_INSTANCE_NAMESPACE: $PARAM_INSTANCE_NAMESPACE]\n\033[0m\n"
+printf "\033[33m [PARAM_CC_NAME: $PARAM_CC_NAME]\n\033[0m\n"
+printf "\033[33m [PARAM_TEMPLATE_NAME: $PARAM_TEMPLATE_NAME]\n\033[0m\n"
+printf "\033[33m [PARAM_ID_FROM_PROVIDER: $PARAM_ID_FROM_PROVIDER]\n\033[0m\n"
+
+
 jq -n '{"ipv4":"4.4.4.4"}'
 #jq -n --arg ipv4 "$IMPORTED_VM_IPV4" '{"ipv4":$ipv4}'
