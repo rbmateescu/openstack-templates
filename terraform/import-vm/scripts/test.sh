6b797e62dc516f097f4ff9955147342933825730
mateescu <mateescu@ca.ibm.com>
2019-04-17 18:10:22 -0400

    we

--- terraform/import-vm/scripts/test.sh
+++ terraform/import-vm/scripts/test.sh
@@ -1,21 +1,3 @@
-# Get script parameters
-eval "$(jq -r '@sh "PARAM_CAM_IP=\(.host_name) 
-PARAM_AUTH_USER=\(.user_name) 
-PARAM_AUTH_PASSWORD=\(.password) 
-PARAM_INSTANCE_NAME=\(.instance_name) 
-PARAM_INSTANCE_NAMESPACE=\(.instance_namespace) 
-PARAM_CC_NAME=\(.cloud_connection_id) 
-PARAM_TEMPLATE_NAME=\(.template_id) 
-PARAM_ID_FROM_PROVIDER=\(.id_from_provider)"')"
-#eval "$(jq -r '@sh "PARAM_CAM_IP=\(.host_name)"')"
-#eval "$(jq -r '@sh "PARAM_AUTH_USER=\(.user)"')"
-#eval "$(jq -r '@sh "PARAM_AUTH_PASSWORD=\(.password)"')"
-#eval "$(jq -r '@sh "PARAM_INSTANCE_NAME=\(.instance_name)"')"
-#eval "$(jq -r '@sh "PARAM_INSTANCE_NAMESPACE=\(.instance_namespace)"')"
-#eval "$(jq -r '@sh "PARAM_CC_NAME=\(.cloud_connection_id)"')"
-#eval "$(jq -r '@sh "PARAM_TEMPLATE_NAME=\(.template_id)"')"
-#eval "$(jq -r '@sh "PARAM_ID_FROM_PROVIDER=\(.id_from_provider)"')"
-
 printf "\033[33m [PARAM_CAM_IP: $PARAM_CAM_IP]\n\033[0m\n"
 printf "\033[33m [PARAM_AUTH_USER: $PARAM_AUTH_USER]\n\033[0m\n"
 printf "\033[33m [PARAM_INSTANCE_NAME: $PARAM_INSTANCE_NAME]\n\033[0m\n"
@@ -105,5 +87,3 @@ function run_cam_import() {
 }
 
 run_cam_import
-jq -n '{"ipv4":"4.4.4.4"}'
-#jq -n --arg ipv4 "$IMPORTED_VM_IPV4" '{"ipv4":$ipv4}'
