7ab69c902c85c17a7a278c5a21088a639445abb9
mateescu <mateescu@ca.ibm.com>
2019-04-17 14:24:49 -0400

    imported_vm_ipv4

--- terraform/import-vm/outputs.tf
+++ terraform/import-vm/outputs.tf
@@ -2,11 +2,11 @@
 #  description = "URL of the imported CAM instance"
 #}
 
-#locals {
-#  ipv4   = "${camc_scriptpackage.FetchIPV4.result}"
-#}
+locals {
+  ipv4   = "${camc_scriptpackage.FetchIPV4.result}"
+}
 
-#output "imported_vm_ipv4"
-#{
-#  value = "${local.ipv4}"
-#}
+output "imported_vm_ipv4"
+{
+  value = "${local.ipv4}"
+}
