f44c4f82c8f09cfde3eb6b2305c5ef1c59586cdb
mateescu <mateescu@ca.ibm.com>
2019-04-17 12:42:47 -0400

    IPV4

--- terraform/import-vm/main.tf
+++ terraform/import-vm/main.tf
@@ -7,6 +7,6 @@ resource "null_resource" "ImportVM" {
 resource "camc_scriptpackage" "FetchIPV4" {
   depends_on = ["null_resource.ImportVM"]
   
-  program = ["cat ./ipv4"]
+  program = ["svrInfo=$(cat ./ipv4);", "if [ -z $svrInfo ]; then echo '{}'; else echo $svrInfo; fi"]
   on_create = true
 }
